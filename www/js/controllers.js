fd86b2ee674e4bfa6e62c895b2dc8310b0dd3757
Chris W Jones <chris@christopherjones.us>
2015-01-24 22:30:29 -0500

    Wireframed everything, still working on logic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,15 @@
 angular.module('starter.controllers', [])
 
-.controller('CalcCtrl', function($scope) {})
+.controller('CalcCtrl', function($scope) {
+	laps = 1
+	time = {
+		minutes: 2,
+		seconds: 2
+	};
+	goallap = ((time.minutes * 60) + seconds) / laps;
+	$scope.laps = laps;
+	$scope.time = time;
+	$scope.goallap = goallap;
+})
 
 .controller('AboutCtrl', function($scope) {});
